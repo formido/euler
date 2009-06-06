6ce2ea348b6cbd4e9139ff4c792d8e1673e4430d
root <root@localhost.localdomain>
2009-06-06 17:46:03 -0400

    euler_1.ml: Oops, this was the real solution. Heh.

--- problem_1/euler_1.ml
+++ problem_1/euler_1.ml
@@ -1,13 +1,14 @@
+let sum =
+  let x = ref 0 in
+  let rec loop i =
+    if i >= 999 then i + !x
+    else if i mod 3 == 0 || i mod 5 == 0 then (
+      x := !x + i;
+      loop (i + 1)
+    )
+    else loop (i + 1)
+  in
+  loop 0
+  ;;
 
-let test = 
-  let x = 0 in
-  let rec loop i s =
-    if i >= 5 then s
-    else (
-      x = x + i; 
-      print_endline (string_of_int x);
-      print_endline (string_of_int i);
-      loop (i+1; i+s)
-  )in
-  loop 0 0
-;;
+print_endline (string_of_int (sum));;
